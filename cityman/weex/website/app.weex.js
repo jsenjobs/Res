dc4218b9187f427de930a33ea8f6d63972e0a79f
jsenjobs <jsen1922279340@163.com>
2017-05-19 19:15:26 +0800

    Add files via upload

---
+++ cityman/weex/website/app.weex.js
@@ -0,0 +1,206 @@
+// { "framework": "Vue" }
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _foo = __webpack_require__(1);
+
+	var _foo2 = _interopRequireDefault(_foo);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	_foo2.default.el = '#root';
+	exports.default = new Vue(_foo2.default);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var __vue_exports__, __vue_options__
+	var __vue_styles__ = []
+
+	/* styles */
+	__vue_styles__.push(__webpack_require__(2)
+	)
+
+	/* script */
+	__vue_exports__ = __webpack_require__(3)
+
+	/* template */
+	var __vue_template__ = __webpack_require__(4)
+	__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+	if (
+	  typeof __vue_exports__.default === "object" ||
+	  typeof __vue_exports__.default === "function"
+	) {
+	if (Object.keys(__vue_exports__).some(function (key) { return key !== "default" && key !== "__esModule" })) {console.error("named exports are not supported in *.vue files.")}
+	__vue_options__ = __vue_exports__ = __vue_exports__.default
+	}
+	if (typeof __vue_options__ === "function") {
+	  __vue_options__ = __vue_options__.options
+	}
+	__vue_options__.__file = "/Users/jsen/Documents/Projects/Weex/tiny/WebBrowser/src/foo.vue"
+	__vue_options__.render = __vue_template__.render
+	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+	__vue_options__._scopeId = "data-v-72049bba"
+	__vue_options__.style = __vue_options__.style || {}
+	__vue_styles__.forEach(function (module) {
+	  for (var name in module) {
+	    __vue_options__.style[name] = module[name]
+	  }
+	})
+	if (typeof __register_static_styles__ === "function") {
+	  __register_static_styles__(__vue_options__._scopeId, __vue_styles__)
+	}
+
+	module.exports = __vue_exports__
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "wrapper": {
+	    "alignItems": "center",
+	    "marginTop": 120
+	  },
+	  "title": {
+	    "fontSize": 48
+	  },
+	  "logo": {
+	    "width": 85,
+	    "height": 81
+	  }
+	}
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+
+	var navigator = weex.requireModule('navigator');
+	var urlReomte = "http://120.25.217.56/wx/";
+	var urlLocal = "http://192.168.1.100:3200/dist/";
+	var gitUrl = "https://raw.githubusercontent.com/jsenjobs/Res/master/cityman/weex/website";
+	var cUrl = gitUrl;
+	exports.default = {
+	  data: {
+	    logoUrl: 'file:///loading.gif',
+	    target: 'World',
+	    page: [{ "name": "主页", "icon": "cityman", "selectedIcon": "citymand", "url": cUrl + "tabmain.weex.js", "navbarTitle": "浏览", "showNavbar": "show", "navbarColor": "#ffff00" }, { "name": "设置", "icon": "tinyfunction", "selectedIcon": "tinyfunctiond", "url": cUrl + "tabset.weex.js", "navbarTitle": "设置", "showNavbar": "show", "navbarColor": "#ff0000" }]
+	  },
+	  computed: {
+	    pageStr: function pageStr() {
+	      return JSON.stringify(this.page);
+	    }
+	  },
+	  methods: {
+	    update: function update(e) {
+	      this.target = 'Weex';
+	      console.log('target:', this.target);
+	    }
+	  },
+	  mounted: function mounted() {
+	    // navigator.setNavBarHidden({'hidden':true,'animated':false});
+	    // navigator.setNavBarTitle({'title':'TinyApp'})
+	    // navigator.setNavBarBackgroundColor({'backgroundColor':'#ffff00'})
+	    // navigator.setNavBarRightItem({'title':'jack','titleColor':'#550099'})
+	    // navigator.setNavBarLeftItem({'icon':'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1494282620164&di=7d3ae4c363b7ca86ea7214a091ae812e&imgtype=0&src=http%3A%2F%2Fpic31.huitu.com%2Fres%2F20150601%2F698885_20150601111256759200_1.jpg'})
+	  }
+	};
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+	module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+	  return _c('div', {
+	    staticClass: ["wrapper"],
+	    on: {
+	      "click": _vm.update
+	    }
+	  }, [_c('image', {
+	    staticClass: ["logo"],
+	    attrs: {
+	      "src": _vm.logoUrl
+	    }
+	  }), _c('navbar', {
+	    attrs: {
+	      "items": _vm.pageStr
+	    }
+	  })], 1)
+	},staticRenderFns: []}
+	module.exports.render._withStripped = true
+
+/***/ })
+/******/ ]);
---
+++ cityman/weex/website/tabmain.weex.js
@@ -0,0 +1,258 @@
+// { "framework": "Vue" }
+
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _tabmain = __webpack_require__(5);
+
+	var _tabmain2 = _interopRequireDefault(_tabmain);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	_tabmain2.default.el = '#tabmain';
+	exports.default = new Vue(_tabmain2.default);
+
+/***/ }),
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */,
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var __vue_exports__, __vue_options__
+	var __vue_styles__ = []
+
+	/* styles */
+	__vue_styles__.push(__webpack_require__(6)
+	)
+
+	/* script */
+	__vue_exports__ = __webpack_require__(7)
+
+	/* template */
+	var __vue_template__ = __webpack_require__(8)
+	__vue_options__ = __vue_exports__ = __vue_exports__ || {}
+	if (
+	  typeof __vue_exports__.default === "object" ||
+	  typeof __vue_exports__.default === "function"
+	) {
+	if (Object.keys(__vue_exports__).some(function (key) { return key !== "default" && key !== "__esModule" })) {console.error("named exports are not supported in *.vue files.")}
+	__vue_options__ = __vue_exports__ = __vue_exports__.default
+	}
+	if (typeof __vue_options__ === "function") {
+	  __vue_options__ = __vue_options__.options
+	}
+	__vue_options__.__file = "/Users/jsen/Documents/Projects/Weex/tiny/WebBrowser/src/tabmain.vue"
+	__vue_options__.render = __vue_template__.render
+	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
+	__vue_options__._scopeId = "data-v-6dbaa07c"
+	__vue_options__.style = __vue_options__.style || {}
+	__vue_styles__.forEach(function (module) {
+	  for (var name in module) {
+	    __vue_options__.style[name] = module[name]
+	  }
+	})
+	if (typeof __register_static_styles__ === "function") {
+	  __register_static_styles__(__vue_options__._scopeId, __vue_styles__)
+	}
+
+	module.exports = __vue_exports__
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+	module.exports = {
+	  "group": {
+	    "flexDirection": "row",
+	    "justifyContent": "space-around"
+	  },
+	  "input": {
+	    "width": 750,
+	    "fontSize": 36,
+	    "borderWidth": 2,
+	    "borderStyle": "solid",
+	    "borderColor": "#BBBBBB"
+	  },
+	  "button": {
+	    "width": 225,
+	    "textAlign": "center",
+	    "backgroundColor": "#D3D3D3",
+	    "fontSize": 30
+	  },
+	  "webview": {
+	    "marginLeft": 0,
+	    "width": 750,
+	    "flex": 1,
+	    "borderWidth": 2,
+	    "borderStyle": "solid",
+	    "borderColor": "#41B883"
+	  }
+	}
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+
+	var webview = weex.requireModule('webview');
+	var modal = weex.requireModule('modal');
+	exports.default = {
+	  data: function data() {
+	    return {
+	      url: 'https://www.baidu.com',
+	      value: 'https://www.baidu.com'
+	    };
+	  },
+
+	  methods: {
+	    loadURL: function loadURL(event) {
+	      var _this = this;
+
+	      this.url = this.value;
+	      modal.toast({ message: 'load url:' + this.url });
+	      setTimeout(function () {
+	        console.log('will go back.');
+	        modal.toast({ message: 'will go back' });
+	        webview.goBack(_this.$refs.webview);
+	      }, 10000);
+	    },
+	    reload: function reload(event) {
+	      console.log('will reload webview');
+	      modal.toast({ message: 'reload' });
+	      webview.reload(this.$refs.webview);
+	    },
+	    start: function start(event) {
+	      console.log('pagestart', event);
+	      modal.toast({ message: 'pagestart' });
+	    },
+	    finish: function finish(event) {
+	      console.log('pagefinish', event);
+	      modal.toast({ message: 'pagefinish' });
+	    },
+	    error: function error(event) {
+	      console.log('error', event);
+	      modal.toast({ message: 'error' });
+	    }
+	  }
+	};
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+	module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+	  return _c('div', {
+	    staticClass: ["wrapper"]
+	  }, [_c('div', {
+	    staticClass: ["group"]
+	  }, [_c('input', {
+	    ref: "input",
+	    staticClass: ["input"],
+	    attrs: {
+	      "type": "url",
+	      "autofocus": "false",
+	      "value": (_vm.value)
+	    },
+	    on: {
+	      "input": function($event) {
+	        _vm.value = $event.target.attr.value
+	      }
+	    }
+	  })]), _c('div', {
+	    staticClass: ["group"]
+	  }, [_c('text', {
+	    staticClass: ["button"],
+	    on: {
+	      "click": _vm.loadURL
+	    }
+	  }, [_vm._v("LoadURL")]), _c('text', {
+	    staticClass: ["button"],
+	    on: {
+	      "click": _vm.reload
+	    }
+	  }, [_vm._v("reload")])]), _c('web', {
+	    ref: "webview",
+	    staticClass: ["webview"],
+	    attrs: {
+	      "src": _vm.url
+	    },
+	    on: {
+	      "pagestart": _vm.start,
+	      "pagefinish": _vm.finish,
+	      "error": _vm.error
+	    }
+	  })], 1)
+	},staticRenderFns: []}
+	module.exports.render._withStripped = true
+
+/***/ })
+/******/ ]);
